e2027bcfb1d421992e0bfa8e6ed7bd24c5e7b63f
Viviane <vivianeaguiarc@outlook.com>
2024-11-10 10:13:49 -0300

    chore: add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="text-red-500">Hello word</h1>
-    </div>
+    <Button>Hello world</Button>
   );
 };
 export default Home;
